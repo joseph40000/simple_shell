be5494c5d4eaa2ebe743224d103729650e139827
amanuel <amanuelnobel10@gmail.com>
2021-12-18 08:37:32 +0000

    env

---
+++ environ.c
@@ -0,0 +1,15 @@
+#include "allhead.h"
+
+/**
+  * print_environ - prints all the environment variables.
+  * Return: nothing.
+  */
+void print_environ(void)
+{
+	int i;
+
+	i = 0;
+	while (environ[i])
+		printf("%s\n", environ[i++]);
+}
+
